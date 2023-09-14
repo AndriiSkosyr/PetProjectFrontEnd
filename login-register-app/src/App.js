f6f31c41e3023b2fd89b3ac07c664f0b3accb689
Andrii <andrewskosyr@gmail.com>
2023-09-14 16:38:32 +0300

    Login/RegisterPage v.0.2.1 Developing functional to send requests from react to an API.

--- login-register-app/src/App.js
+++ login-register-app/src/App.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Login } from './Login';
-import { Register } from './Register';
+import { Login } from './components/Login'
+import { Register } from './components/Register';
 
 function App() {
   const [currentForm, setCurrentForm] = useState("login");
 
   const toggleform = (formName) => {
     setCurrentForm(formName);
+  }
+
+  const ClientRegister = (name, email, pass) => {
+    fetch("http://127.0.0.1:5000/register", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        pass: pass,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
   }
 
   return (
--- login-register-app/src/components/Register.jsx
+++ login-register-app/src/components/Register.jsx
@@ -8,6 +8,10 @@ export const Register = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email);
+    props.Register(name, email, pass)
+    setName("")
+    setEmail("")
+    setPass("")
   }
 
   return (
@@ -20,7 +24,7 @@ export const Register = (props) => {
         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
         <label htmlFor="password">Password</label>
         <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-        <button type="submit">Log In</button>
+        <button type="submit">Register</button>
       </form>
       <button className="link-btn" onClick={() => props.onFormSwitch("login")}>Already have an account? Login here.</button>
     </div>
