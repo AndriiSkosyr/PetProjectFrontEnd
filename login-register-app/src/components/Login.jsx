14963445260b3e17754f64bcd140b8547f2660a6
Andrii <andrewskosyr@gmail.com>
2023-09-26 11:30:32 +0300

    Login/RegisterPage v.0.2.5 Both Login and Register pages work fine and send correct data to an API.

--- login-register-app/src/components/Login.jsx
+++ login-register-app/src/components/Login.jsx
@@ -1,22 +1,39 @@
 import React, { useState } from "react";
 
 export const Login = (props) => {
-  const [email, setEmail] = useState("")
-  const [pass, setPass] = useState("")
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email);
+    ClientLogin(name, password)
+  }
+
+  const ClientLogin = async (name, password) => {
+    await fetch("http://127.0.0.1:5000/login", {
+      method: "POST",
+      body: JSON.stringify({
+        clientName: name,
+        clientPassword: password
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .catch((err) => {
+        console.log(err.message);
+      });
   }
 
   return (
     <div className="auth-form-container">
       <h2>Login</h2>
       <form className="login-form" onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+        <label htmlFor="name">Email</label>
+        <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Your Name" id="name" name="name" />
         <label htmlFor="password">Password</label>
-        <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
         <button type="submit">Log In</button>
       </form>
       <button className="link-btn" onClick={() => props.onFormSwitch("register")}>Don't have an account? Register here.</button>
--- login-register-app/src/components/Register.jsx
+++ login-register-app/src/components/Register.jsx
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
 
-
 export const Register = (props) => {
   const [email, setEmail] = useState("")
-  const [password, setPass] = useState("")
+  const [password, setPassword] = useState("")
   const [name, setName] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email);
     ClientRegister(name, email, password)
     //setName("")
     //setEmail("")
-    //setPass("")
+    //setPassword("")
   }
 
   const ClientRegister = async (name, email, password) => {
@@ -38,12 +36,12 @@ export const Register = (props) => {
     <div className="auth-from-container">
       <h2>Register</h2>
       <form className="register-form" onSubmit={handleSubmit}>
-        <label htmlFor="name">FullName</label>
-        <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="yourname" id="name" name="name"></input>
+        <label htmlFor="name">Full name</label>
+        <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Your Name" id="name" name="name"></input>
         <label htmlFor="email">Email</label>
         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
         <label htmlFor="password">Password</label>
-        <input value={password} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
         <button type="submit">Register</button>
       </form>
       <button className="link-btn" onClick={() => props.onFormSwitch("login")}>Already have an account? Login here.</button>
