073623beac1c3ca800aae5ccca8b20bc97a9b00d
Andrii <andrewskosyr@gmail.com>
2023-10-05 13:36:30 +0300

    Navbar v.0.2 Created flexible navbar with 5 buttons and pop-up button for menu

---
+++ pet-project-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './Pages/home';
+import About from './Pages/about';
+import Settings from './Pages/settings';
+import Profile from './Pages/profile';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/settings" exact element={<Settings />} />
+        <Route path="/profile" exact element={<Profile />} />
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
